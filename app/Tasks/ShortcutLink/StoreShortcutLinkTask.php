5743d23a65cd8b32f8d3335bad3c44a0bb7f3809
Said ibrahim <saidibrahim73@hotmail.com>
2023-03-27 03:51:45 +0100

    if count ShortcutLink=20 delete latest item

--- app/Tasks/ShortcutLink/StoreShortcutLinkTask.php
+++ app/Tasks/ShortcutLink/StoreShortcutLinkTask.php
@@ -1,12 +1,17 @@
 <?php
 namespace App\Tasks\ShortcutLink;
 
+use App\Actions\ShortcutLink\DeleteShortcutLinkAction;
 use App\Http\Requests\ShortcutLinkRequest;
 use App\Models\ShortcutLink;
 
 class StoreShortcutLinkTask
 {
     public function run(ShortcutLinkRequest $request){
+        if(ShortcutLink::count()>=20){
+            $lastShortcutLink=ShortcutLink::latest()->first();
+            app(DeleteShortcutLinkAction::class)->run($lastShortcutLink);
+        }
         return ShortcutLink::create($request->validated()+['user_id'=>auth()->ID()]);
     }
 }
